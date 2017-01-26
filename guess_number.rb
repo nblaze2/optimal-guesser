bfa5cc2310d411e48d9001d3912c0344b9c17180
Nick Blaisdell <nick.blaisdell@gmail.com>
2017-01-26 16:38:14 -0500

    last

--- guess_number.rb
+++ guess_number.rb
@@ -1,23 +1,30 @@
+
 def guess_number(min, max)
-  You can call the `check` method with a number to see if it
-  is the hidden value.
+  # You can call the `check` method with a number to see if it
+  # is the hidden value.
+  #
+  # If the guess is correct, it will return 0.
+  # If the guess is too high, it will return 1.
+  # If the guess is too low, it will return -1.
+  #
+  # If you call `check` too many times, the program will crash.
+  #
+  # e.g. if the hidden number is 43592, then
+  #
+  # check(50000) # => 1
+  # check(40000) # => -1
+  # check(43592) # => 0
+  #
+  # When you've figured out what the hidden number is, return it
+  # from this method.
+  puts middle = (min + max) / 2
+  guess = check(middle)
 
-  If the guess is correct, it will return 0.
-  If the guess is too high, it will return 1.
-  If the guess is too low, it will return -1.
-
-  If you call `check` too many times, the program will crash.
-
-  e.g. if the hidden number is 43592, then
-
-  check(50000) # => 1
-  check(40000) # => -1
-  check(43592) # => 0
-
-  When you've figured out what the hidden number is, return it
-  from this method.
-
-  ######################
-  YOUR CODE GOES HERE #
-  ######################
+  if guess == 0
+    middle
+  elsif guess == -1
+    guess_number(middle, max)
+  elsif guess == 1
+    guess_number(min, middle)
+  end
 end
